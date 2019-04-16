1c84ef90288995dcdcba8b339d87fbeda057ed91
Alex House <alexhouse@0587383724.wireless.umich.net>
2019-04-16 14:22:39 -0400

    TESTING PUSH

--- API_Info.py
+++ API_Info.py
@@ -1,1 +1,10 @@
-import requests
+#spotify keys
+spotify_client_ID = "150d57fa8da244de9fd92f9183069765"
+spotify_client_secret = "451cbccc65ae478e8dfa0d3157ce01f6"
+spotify_access_token = "BQBN_Otnf7A3ECrUn0kQe0E3xLlf6Nu3RmLIPn"
+
+
+#Genius Keys
+genius_client_ID = "krsdtkDIukGbfpfUQeXdGouqdD0oF_yzjKC5C0DFl7xEQB6RbHnMIzbutQuAkVET"
+genius_client_secret = "nWeAPCPkyvefwke-dHyFEK57N9zqD2EgyB94M6jinpEg1lXc_txbwjxd0VH5Y8BFrz9zkahhiqd-8oG84s0LrA"
+genius_access_token = "XT-fiPUp3L_kUjPo3pUZvHH1KZbGqF7u1IlTILxMZFKhff6c_3aQ2fhMvqa3Gj31"
--- SpotifyGenius_data.py
+++ SpotifyGenius_data.py
@@ -1,1 +1,21 @@
-import requests
+import API_Info
+import requests 
+import json
+import spotipy
+import oauthlib
+
+#importing info from API_info.py (Spotify and Genius keys)
+spotify_client_ID = API_Info.spotify_client_ID
+spotify_client_secret = API_Info.spotify_client_secret
+genius_client_ID = API_Info.genius_client_ID
+genius_access_token = API_Info.spotify_access_token
+genius_client_secret = API_Info.genius_client_secret
+genius_access_token = API_Info.genius_access_token
+
+
+# Set up your authentication to Spotify
+auth = spotipy.OAuthHandler(spotify_client_ID, spotify_client_secret)
+
+# Set up your authentication to Genius
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
