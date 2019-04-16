4f3913951674e61cf7cf2b6da11bf3ca3d4ca477
Alex House <alexhouse@0587383724.wireless.umich.net>
2019-04-16 15:20:50 -0400

    update

--- API_Info.py
+++ API_Info.py
@@ -2,7 +2,7 @@
 spotify_client_ID = "150d57fa8da244de9fd92f9183069765"
 spotify_client_secret = "451cbccc65ae478e8dfa0d3157ce01f6"
 spotify_access_token = "BQBN_Otnf7A3ECrUn0kQe0E3xLlf6Nu3RmLIPn"
-
+spotify_redirect_URI = "http://localhost:1245367208"
 
 #Genius Keys
 genius_client_ID = "krsdtkDIukGbfpfUQeXdGouqdD0oF_yzjKC5C0DFl7xEQB6RbHnMIzbutQuAkVET"
--- SpotifyGenius_data.py
+++ SpotifyGenius_data.py
@@ -2,20 +2,28 @@ import API_Info
 import requests 
 import json
 import spotipy
-import oauthlib
+from spotipy import oauth2
 
 #importing info from API_info.py (Spotify and Genius keys)
 spotify_client_ID = API_Info.spotify_client_ID
 spotify_client_secret = API_Info.spotify_client_secret
+spotify_access_token = API_Info.spotify_access_token
+spotify_redirect_URI = API_Info.spotify_redirect_URI
+
 genius_client_ID = API_Info.genius_client_ID
-genius_access_token = API_Info.spotify_access_token
 genius_client_secret = API_Info.genius_client_secret
 genius_access_token = API_Info.genius_access_token
 
+# Set up authentication to Spotify
+sp_auth = oauth2.SpotifyOAuth(spotify_client_ID, spotify_client_secret, spotify_redirect_URI)
+sp = spotipy.Spotify(spotify_access_token)
 
-# Set up your authentication to Spotify
-auth = spotipy.OAuthHandler(spotify_client_ID, spotify_client_secret)
+results = sp.current_user()
+print(results)
 
-# Set up your authentication to Genius
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+
+
+
+# # Set up your authentication to Genius
+# auth = spotipy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
